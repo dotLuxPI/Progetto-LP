8afe15acffe7480fc173d6b182dce542ebfdc1be
Andrea Magliani <114879704+Magliaa@users.noreply.github.com>
2023-12-29 11:59:20 +0100

    :sparkles: add inheritance in def_class and make

--- OOL - OOP/Prolog/oop.pl
+++ OOL - OOP/Prolog/oop.pl
@@ -2,7 +2,7 @@
 %%%% Picco Nicolas 894588
 %%%% Perego Luca 894448
 
-
+%%%% DEF_CLASS PREDICATE
 def_class(Classname, Parents) :-
     def_class(Classname, Parents, []).
 def_class(Classname, Parents, Parts) :-
@@ -10,9 +10,10 @@ def_class(Classname, Parents, Parts) :-
           class_already_defined, fail),
     parents_check(Parents),
     parts_check(Parts),
-    concat_parts(Parts, Parents, [], Result),
+    concat_parts(Parts, Parents, Result),
     assertz(class(Classname, Parents, Result)).
 
+%%%% DEF_CLASS UTILS
 classname_check(Classname) :-
     atom(Classname),
     clause(class(Classname, _, _), _),
@@ -52,61 +53,72 @@ is_method(Method) :-
     is_list(Args),
     callable(Form).
 
-concat_parts(Parts, Parents, _, Result) :-
-    append(Parts, [], Result),
-    check_parents_parts(Parents, Result, Result).
+concat_parts(Parts, Parents, Result) :-
+    append(Parts, [], TempResult),
+    check_parents_parts(Parents, TempResult, Result).
 
-check_parents_parts([], _, _Result) :- !.
+check_parents_parts([], NewParts, NewParts) :- !.
 check_parents_parts([H | T], NewParts, Result) :-
-    findall(Parts, class(H, _, Parts), Parts),
-    flatten(Parts, FlatParts),
-    append_parts(NewParts, FlatParts, Result),
-    write("check_parents_parts: "),
-    writeln(Result),
-    check_parents_parts(T, Result, Result).
+    findall(Parts, class(H, _, Parts), PartsList),
+    flatten(PartsList, FlatParts),
+    append_parts(NewParts, FlatParts, UpdatedNewParts),
+    check_parents_parts(T, UpdatedNewParts, Result).
 
-
-
-append_parts(NewParts, [], Result) :-
-    Result is NewParts,
-    write("append_parts: "),
-    writeln(Result),
+append_parts(NewParts, [], NewParts) :-
     !.
-append_parts(NewParts, [field(Key, _Value)| Tail], Result) :-
+append_parts(NewParts, [field(Key, _Value) | Tail], Result) :-
     memberchk(field(Key, _), NewParts),
-    append_parts(NewParts, Tail, Result).
+    append_parts(NewParts, Tail, Result),
+    !.
+append_parts(NewParts, [field(Key, _Value) | Tail], Result) :-
+    memberchk(field(Key, _, _), NewParts),
+    append_parts(NewParts, Tail, Result),
+    !.
+append_parts(NewParts, [field(Key, _Value, _Type) | Tail], Result) :-
+    memberchk(field(Key, _, _), NewParts),
+    append_parts(NewParts, Tail, Result),
+    !.
+append_parts(NewParts, [field(Key, _Value, _Type) | Tail], Result) :-
+    memberchk(field(Key, _), NewParts),
+    append_parts(NewParts, Tail, Result),
+    !.
 append_parts(NewParts, [method(Key, _, _) | Tail], Result) :-
     memberchk(method(Key, _, _), NewParts),
-    append_parts(NewParts, Tail, Result).
+    append_parts(NewParts, Tail, Result),
+    !.
 append_parts(NewParts, [field(Key, Value) | Tail], Result) :-
-    \+ memberchk(field(Key, _), NewParts),
-    \+ memberchk(method(Key, _, _), NewParts),
     append(NewParts, [field(Key, Value)], UpdatedNewParts),
-    append(Result, [field(Key, Value)], UpdatedNewResult),
-    append_parts(UpdatedNewParts, Tail, UpdatedNewResult).
+    append_parts(UpdatedNewParts, Tail, Result),
+    !.
+append_parts(NewParts, [field(Key, Value, Type) | Tail], Result) :-
+    append(NewParts, [field(Key, Value, Type)], UpdatedNewParts),
+    append_parts(UpdatedNewParts, Tail, Result),
+    !.
 append_parts(NewParts, [method(Key, Args, Form) | Tail], Result) :-
-    \+ memberchk(field(Key, _), NewParts),
-    \+ memberchk(method(Key, _, _), NewParts),
     append(NewParts, [method(Key, Args, Form)], UpdatedNewParts),
-    append_parts(UpdatedNewParts, Tail, Result).
+    append_parts(UpdatedNewParts, Tail, Result),
+    !.
 
 
 
-make(InstanceName, ClassName) :-
-    make(InstanceName, ClassName, []).
-make(InstanceName, ClassName, ParameterList) :-
+%%%% MAKE PREDICATE
+make(InstanceName, Classname) :-
+    make(InstanceName, Classname, []).
+make(InstanceName, Classname, ParameterList) :-
     catch(is_valid_instancename(InstanceName),
           instance_found, fail),
-    clause(class(ClassName, _, _), _),
+    clause(class(Classname, _, _), _),
     is_list(ParameterList),
     is_parameter(ParameterList),
-    assertz(instanceof(InstanceName, ClassName, ParameterList)).
-make(InstanceName, ClassName, ParameterList) :-
-    clause(class(ClassName, _, _), _),
+    concat_parameter(ParameterList, Classname, FinalParameter),
+    assertz(instance(InstanceName, Classname, FinalParameter)).
+make(InstanceName, Classname, ParameterList) :-
+    clause(class(Classname, _, _), _),
     is_list(ParameterList),
     is_parameter(ParameterList),
     catch(is_var(InstanceName), var, write("variabile non istanziata")).
 
+%%%% MAKE UTILS
 is_var(X) :-
     var(X),
     throw(var).
@@ -115,7 +127,7 @@ is_var(X) :-
 
 is_valid_instancename(InstanceName) :-
     atom(InstanceName),
-    clause(instanceof(InstanceName, _, _), _),
+    clause(instance(InstanceName, _, _), _),
     write('The instance already exists!'),
     throw(instance_found).
 is_valid_instancename(InstanceName) :-
@@ -130,34 +142,91 @@ is_parameter_check(Field = Value) :-
     atom(Field),
     atomic(Value).
 
+concat_parameter(ParameterList, Classname, FinalParameter) :-
+    findall(class(Classname, Parents, Parts),
+            class(Classname, Parents, Parts),
+            [class(Classname, Parents, Parts) | _T]),
+    append_parameter(ParameterList, Parts, FinalParameter).
 
+append_parameter(ParameterList, [], ParameterList) :-
+    !.
+append_parameter(ParameterList, [field(Key, _Value) | T], FinalParameter) :-
+    memberchk(Key = _, ParameterList),
+    append_parameter(ParameterList, T, FinalParameter),
+    !.
+append_parameter(ParameterList, [field(Key, _Value, _Type) | T], FinalParameter) :-
+    memberchk(Key = _, ParameterList),
+    append_parameter(ParameterList, T, FinalParameter),
+    !.
+append_parameter(ParameterList, [method(_Key, _Args, _Form) | T], FinalParameter) :-
+    append_parameter(ParameterList, T, FinalParameter),
+    !.
+append_parameter(ParameterList, [field(Key, Value) | T], FinalParameter) :-
+    append(ParameterList, [Key = Value], IntermediateParameter),
+    append_parameter(IntermediateParameter, T, FinalParameter),
+    !.
+append_parameter(ParameterList, [field(Key, Value, _Type) | T], FinalParameter) :-
+    append(ParameterList, [Key = Value], IntermediateParameter),
+    append_parameter(IntermediateParameter, T, FinalParameter),
+    !.
+
+
+
+%%%% IS_CLASS PREDICATE
 is_class(Classname) :-
     atom(Classname),
     clause(class(Classname, _, _), _).
 
 
+
+%%%% IS_INSTANCE PREDICATE
 is_instance(Value) :-
     is_instance(Value, _).
 is_instance(Value) :-
     clause(Value, _).
 is_instance(Value, Classname) :-
-    clause(instanceof(Value, Classname, _), _).
+    clause(instance(Value, Classname, _), _).
 
 
-inst(InstanceName, Instance) :-
-    bagof(instanceof(InstanceName, Classname, ParameterList),
-          instanceof(InstanceName, Classname, ParameterList),
-          Instance),
+
+%%%% INST PREDICATE
+inst(InstanceName, H) :-
+    bagof(instance(InstanceName, Classname, ParameterList),
+          instance(InstanceName, Classname, ParameterList),
+          [H | _T]),
     !.
 inst(_InstanceName, _Instance) :-
     write("Instance not found!"),
     fail.
 
 
+
+%%%% FIELD PREDICATE
 field(Instance, FieldName, Result) :-
     atom(Instance),
-    field(inst(Instance), FieldName, Result).
-field(_Instance, FieldName, _Result) :-
-    atom(FieldName).
+    inst(Instance, Inst),
+    field(Inst, FieldName, Result).
+field(instance(_InstanceName, _Classname, ParameterList), FieldName, Result) :-
+    atom(FieldName),
+    memberchk(FieldName = Value, ParameterList),
+    Result = Value.
+
+
+
+%%%% FIELDX PREDICATE
+fieldx(Instance, FieldNames, Result) :-
+    atom(Instance),
+    is_list(FieldNames),
+    inst(Instance, Inst),
+    fieldx(Inst, FieldNames, Result).
+fieldx(_Instance, [], _Result) :-
+    write('The field names list is empty!'),
+    !.
+fieldx(Instance, [H], Result) :-
+    field(Instance, H, Result),
+    !.
+fieldx(Instance, [H | T], Result) :-
+    field(Instance, H, _),
+    fieldx(Instance, T, Result).
 
 
